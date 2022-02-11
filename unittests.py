63055df2638368f8af3b09e7eb843b6571c5664b
AllenNotAlan <allenloyola23@gmail.com>
2022-02-11 13:57:33 +0000

    restructure files

--- unittests.py
+++ unittests.py
@@ -1,16 +1,16 @@
 import unittest
-import main
+import brailleSolution
 
 class TestStringMethods(unittest.TestCase):
 
     def test_word(self):
-        self.assertEqual(main.solution("code"), "100100101010100110100010")
+        self.assertEqual(brailleSolution.solution("code"), "100100101010100110100010")
 
     def test_word_with_capitals(self):
-        self.assertEqual(main.solution("Braille"), "000001110000111010100000010100111000111000100010")
+        self.assertEqual(brailleSolution.solution("Braille"), "000001110000111010100000010100111000111000100010")
         
     def test_sentence(self):
-        self.assertEqual(main.solution("The quick brown fox jumps over the lazy dog"),
+        self.assertEqual(brailleSolution.solution("The quick brown fox jumps over the lazy dog"),
          "000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110")
 
 if __name__ == '__main__':
